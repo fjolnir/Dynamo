9026c26ff5aa72404a4bb09a2e730ca25a91b736
Fjölnir Ásgeirsson <me@fyolnish.com>
2011-11-23 01:26:28 +0900

    Added some clarifications

--- engine/texture.h
+++ engine/texture.h
@@ -9,9 +9,10 @@ typedef struct _Texture {
 	vec2_t size;
 } Texture_t;
 
-// A convenience structure to specify areas to sample from a texture (in UV coordinates)
+// A  structure to specify areas to sample from a texture (in UV coordinates)
 typedef union _TextureRect {
 	vec4_t v;
+	float *f;
 	struct {
 		vec2_t origin;
 		vec2_t size;
@@ -29,5 +30,6 @@ extern void texture_destroy(Texture_t *aTexture);
 // Generates a UV texture rectangle from pixel coordinates
 extern TextureRect_t textureRectangle_createWithPixelCoordinates(Texture_t *aTexture, vec2_t aOrigin, vec2_t aSize);
 extern TextureRect_t textureRectangle_createWithSizeInPixels(Texture_t *aTexture, vec2_t aSize);
+// Takes UV (0.0-1.0) coordinates
 extern TextureRect_t textureRectangle_create(float aX, float aY, float aWidth, float aHeight);
 #endif
