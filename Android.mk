dd96e80a0d2e4843ba4a2013400daee3d6249813
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2012-05-16 12:52:11 +0900

    Made chipmunk use floats on android

--- Android.mk
+++ Android.mk
@@ -1,10 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_CFLAGS := -I$(LOCAL_PATH)/Dependencies -std=gnu99 -I$(LOCAL_PATH)/Dependencies/png -I$(LOCAL_PATH)/Dependencies/mxml  -g -DDYNAMO_DEBUG
+DEPS_PATH = $(LOCAL_PATH)/Dependencies
+LOCAL_CFLAGS := -I$(DEPS_PATH) -std=gnu99 -I$(DEPS_PATH)/png -I$(DEPS_PATH)/mxml -I$(DEPS_PATH)/Chipmunk/include -I$(DEPS_PATH)/Chipmunk/include/chipmunk  -g -DDYNAMO_DEBUG -DCP_USE_DOUBLES=0
 LOCAL_CXXFLAGS := -std=gnu++98
 LOCAL_ARM_MODE := arm
-TARGET_PLATFORM := armeabi-v7a
+TARGET_PLATFORM := android-9
 TARGET_ABI := android-9-armeabi
 
 LOCAL_MODULE    := dynamo
@@ -29,8 +30,8 @@ Source/sound_android.c \
 Source/dictionary.c \
 Source/json.c \
 Source/primitive_types.c \
+Source/world.c \
 Dependencies/GLMath/GLMath.c \
-Dependencies/GLMath/GLMathExports.c \
 Dependencies/GLMath/GLMathUtilities.c \
 Dependencies/mxml/mxml-attr.c \
 Dependencies/mxml/mxml-entity.c \
@@ -65,9 +66,39 @@ Dependencies/yajl/yajl_gen.c \
 Dependencies/yajl/yajl_lex.c \
 Dependencies/yajl/yajl_parser.c \
 Dependencies/yajl/yajl_tree.c \
-Dependencies/yajl/yajl_version.c
+Dependencies/yajl/yajl_version.c \
+Dependencies/Chipmunk/src/chipmunk.c \
+Dependencies/Chipmunk/src/constraints/cpConstraint.c \
+Dependencies/Chipmunk/src/constraints/cpDampedRotarySpring.c \
+Dependencies/Chipmunk/src/constraints/cpDampedSpring.c \
+Dependencies/Chipmunk/src/constraints/cpGearJoint.c \
+Dependencies/Chipmunk/src/constraints/cpGrooveJoint.c \
+Dependencies/Chipmunk/src/constraints/cpPinJoint.c \
+Dependencies/Chipmunk/src/constraints/cpPivotJoint.c \
+Dependencies/Chipmunk/src/constraints/cpRatchetJoint.c \
+Dependencies/Chipmunk/src/constraints/cpRotaryLimitJoint.c \
+Dependencies/Chipmunk/src/constraints/cpSimpleMotor.c \
+Dependencies/Chipmunk/src/constraints/cpSlideJoint.c \
+Dependencies/Chipmunk/src/cpArbiter.c \
+Dependencies/Chipmunk/src/cpArray.c \
+Dependencies/Chipmunk/src/cpBB.c \
+Dependencies/Chipmunk/src/cpBBTree.c \
+Dependencies/Chipmunk/src/cpBody.c \
+Dependencies/Chipmunk/src/cpCollision.c \
+Dependencies/Chipmunk/src/cpHashSet.c \
+Dependencies/Chipmunk/src/cpPolyShape.c \
+Dependencies/Chipmunk/src/cpShape.c \
+Dependencies/Chipmunk/src/cpSpace.c \
+Dependencies/Chipmunk/src/cpSpaceComponent.c \
+Dependencies/Chipmunk/src/cpSpaceHash.c \
+Dependencies/Chipmunk/src/cpSpaceQuery.c \
+Dependencies/Chipmunk/src/cpSpaceStep.c \
+Dependencies/Chipmunk/src/cpSpatialIndex.c \
+Dependencies/Chipmunk/src/cpSweep1D.c \
+Dependencies/Chipmunk/src/cpVect.c
 
 LIBS+=bps screen EGL GLESv2 freetype
 LOCAL_LDLIBS := -lz -llog -ldl -lGLESv2 -lOpenSLES
 
 include $(BUILD_SHARED_LIBRARY)
+
